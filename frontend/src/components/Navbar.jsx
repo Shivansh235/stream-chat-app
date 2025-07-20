3201e72a736cc18b0ae543f896300c298adde260
Shivansh235 <shivansh.2023ug4027@iiitranchi.ac.in>
2025-07-20 13:08:20 +0530

    changes in themeselector

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import useAuthUser from "../hooks/useAuthUser";
 import { BellIcon, LogOutIcon, ShipWheelIcon } from "lucide-react";
-import ThemeSelector from "./ThemeSelector.jsx";
+import ThemeSelector from "./ThemeSelector";
 import useLogout from "../hooks/useLogout";
 
 const Navbar = () => {
