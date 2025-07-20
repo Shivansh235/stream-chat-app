7fc96d178a354c2ceb915d0372cf06272c782f24
Shivansh235 <shivansh.2023ug4027@iiitranchi.ac.in>
2025-07-20 13:27:03 +0530

    Changes server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000")
   ConnDB();
 })
