77fe6f5e7eb81b15c5d14fc5588c4ff6fa7f4892
Shivansh235 <shivansh.2023ug4027@iiitranchi.ac.in>
2025-07-20 16:23:43 +0530

    Changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,6 +28,10 @@ app.use("/api/auth",authRoutes);
 app.use("/api/user",userRoutes); 
 app.use("/api/chat",chatRoutes); 
 
+app.get("/" , (req,res)=>{
+  res.send("Hello world!")
+})
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
